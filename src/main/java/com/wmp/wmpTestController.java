400c183fdd30c2dd986066eec4efb8ad8b759585
user <user@DESKTOP-151RT89>
2019-08-22 11:06:39 +0900

    wmptest start

---
+++ src/main/java/com/wmp/wmpTestController.java
@@ -0,0 +1,15 @@
+package com.wmp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class wmpTestController {
+	@RequestMapping("/")
+    public String showHomePage(Model m) {
+        m.addAttribute("name", "Hello");
+        return "wmptest";
+    }
+	
+}
